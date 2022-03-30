d47a268902d0a0b7191596e97ec62872a3bb1a7a
rishitagrover16 <85683679+rishitagrover16@users.noreply.github.com>
2022-03-30 16:01:56 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,13 @@
+import plotly.express as px
+import csv
+with open("students.csv") as csv_file:
+  df=csv.DictReader(csv_file)
+  fig=px.scatter(df,x="Marks In Percentage",y="Days Present")
+  fig.show()
+
+  import plotly.express as px
+import csv 
+with open("coffee.csv")as csv_file:
+  df=csv.DictReader(csv_file)
+  fig=px.scatter(df,x="Coffee in ml",y="sleep in hours")
+  fig.show()
